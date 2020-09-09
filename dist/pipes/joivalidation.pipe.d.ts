dfedf2f485511ffe8396bc71fa3774f62ff1c365
Snehlata <snelatag142@gmail.com>
2020-09-09 21:58:31 +0530

    Change Password fixed

--- dist/pipes/joivalidation.pipe.d.ts
+++ dist/pipes/joivalidation.pipe.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="hapi__joi" />
 import { PipeTransform } from '@nestjs/common';
 import { ObjectSchema } from '@hapi/joi';
 export declare class JoiValidationPipe implements PipeTransform {
--- dist/services/email.service.d.ts
+++ dist/services/email.service.d.ts
@@ -5,9 +5,9 @@ export declare class EmailService {
     sendEmail(to: any, subject: any, text: any): Promise<void>;
     gettemplatePaths(templateName: any): string;
     getFileData(templateName: any): string;
-    getHandlebarTemplate(templateName: any): any;
-    getTemlateVerificationLinkTemplate(user: any, link: any): any;
-    getTemlateResetPasswordLinkTemplate(user: any, link: any): any;
+    getHandlebarTemplate(templateName: any): HandlebarsTemplateDelegate<any>;
+    getTemlateVerificationLinkTemplate(user: any, link: any): string;
+    getTemlateResetPasswordLinkTemplate(user: any, link: any): string;
     sendVerificationLink(user: any, link: any): Promise<void>;
     sendResetPasswordLink(user: any, link: any): Promise<void>;
     sendUpdatedPasswordNotification(userModel: any, currentPassword: any, link: any): Promise<void>;
