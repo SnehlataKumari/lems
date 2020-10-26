8ba7edb9b028aee1fe540ef332c6d8e69cc38bc9
sg1995 <satish.gupta@clik.ai>
2020-10-26 13:20:27 +0530

    Add product

--- dist/controllers/index.d.ts
+++ dist/controllers/index.d.ts
@@ -13,5 +13,5 @@ import { LeaveController } from './leave.controller';
 import { DocumentsController } from './documents.controller';
 import { ImportController } from './import.controller';
 import { LiveClassController } from './liveClass.controller';
-declare const _default: (typeof ProductsController | typeof UsersController | typeof ClassesController | typeof AssetsController | typeof AuthController | typeof ChaptersController | typeof SubjectsController | typeof PaymentsController | typeof TeachersController | typeof StudentsController | typeof TestController | typeof LeaveController | typeof DocumentsController | typeof ImportController | typeof LiveClassController)[];
+declare const _default: (typeof UsersController | typeof ClassesController | typeof AssetsController | typeof AuthController | typeof ChaptersController | typeof SubjectsController | typeof PaymentsController | typeof TeachersController | typeof ProductsController | typeof StudentsController | typeof TestController | typeof LeaveController | typeof DocumentsController | typeof ImportController | typeof LiveClassController)[];
 export default _default;
